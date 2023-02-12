3e42ff927f98d3e9b2d48ce5b51f63d47bfecac9
Vardhan <vardhandeepak1234@gmail.com>
2023-02-12 06:01:53 +0530

    order

--- client/src/components/Order.js
+++ client/src/components/Order.js
@@ -3,8 +3,9 @@ const Order = () => {
   return (
     <>
       <div className="row border-2 rounded-lg overflow-hidden shadow-lg  mt-5 mb-5 h-20 bg-gradient-to-br from-pink-200  via-purple-300 to-blue-200">
-        <div className="grid grid-cols-11 mt-3">
-          <div className="row">
+        <div className="row">
+          <div className="grid grid-cols-11 mt-3">
+
             <div>Symbol</div>
             <div>series</div>
             <div>Bond Type</div>
@@ -18,17 +19,19 @@ const Order = () => {
             <div>Maturity Date</div>
           </div>
           <div className="row">
-            <div>NHAI</div>
-            <div>N6</div>
-            <div>Regular</div>
-            <div>8.75</div>
-            <div>1000.00</div>
-            <div>1242.00</div>
-            <div>-0.01</div>
-            <div>31977</div>
-            <div>39,634,532.19</div>
-            <div>CRISIL AAA STABLE</div>
-            <div>5-Feb-29</div>
+            <div className="grid grid-cols-11 mt-3">
+              <div>NHAI</div>
+              <div>N6</div>
+              <div>Regular</div>
+              <div>8.75</div>
+              <div>1000.00</div>
+              <div>1242.00</div>
+              <div>-0.01</div>
+              <div>31977</div>
+              <div>39,634,532.19</div>
+              <div>CRISIL AAA STABLE</div>
+              <div>5-Feb-29</div>
+            </div>
           </div>
         </div>
       </div>
